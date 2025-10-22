b4c3a23b9ea4db3779262a76c112ab2032c4e040
sotogito <sotogitoarchive@gmail.com>
2025-10-23 00:27:23 +0900

    test : Cars 우승 자동차 이름 반환 테스트

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -1,21 +1,49 @@
 package racingcar.domain;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 class CarsTest {
+
+    @Test
+    void 단일_우승_자동차_이름_반환() {
+        Car car1 = new Car("가");
+        Car car2 = new Car("나");
+        Car car3 = new Car("다");
+        Cars cars = new Cars(List.of(car1, car2, car3));
+
+        car1.move();
+        List<String> actual = cars.getWinningCarNames();
+        List<String> expected = List.of("가");
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void 공동_우승_자동차_이름_반황() {
+        Car car1 = new Car("가");
+        Car car2 = new Car("나");
+        Car car3 = new Car("다");
+        Cars cars = new Cars(List.of(car1, car2, car3));
+
+        car1.move();
+        car3.move();
+        List<String> actual = cars.getWinningCarNames();
+        List<String> expected = List.of("가", "다");
+
+        assertThat(actual).isEqualTo(expected);
+    }
 
     @Test
     void 최소_자동차_수_예외처리() {
         List<Car> cars = List.of();
 
         assertSimpleTest(() ->
-                assertThatThrownBy(() -> new Racing(cars, 1))
+                assertThatThrownBy(() -> new Cars(cars))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
@@ -29,18 +57,7 @@ class CarsTest {
         List<Car> cars = List.of(car1, car2, car3);
 
         assertSimpleTest(() ->
-                assertThatThrownBy(() -> new Racing(cars, 1))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = {0, 1000000})
-    void 시도횟수_범위_예외처리(int attempt) {
-        List<Car> cars = List.of(new Car("기"));
-
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> new Racing(cars, attempt))
+                assertThatThrownBy(() -> new Cars(cars))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
