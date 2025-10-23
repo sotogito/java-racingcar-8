2853c49a02bb3d9e18510516d38ece04bc065a5b
sotogito <sotogitoarchive@gmail.com>
2025-10-23 23:49:59 +0900

    test : Cars 랜덤 숫자 리스트에 따른 자동차들 전진 테스트

--- src/test/java/racingcar/domain/car/CarsTest.java
+++ src/test/java/racingcar/domain/car/CarsTest.java
@@ -4,10 +4,26 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class CarsTest {
+
+    @Test
+    void 랜덤숫자에_따른_자동차등_전진() {
+        Car car1 = new Car("가");
+        Car car2 = new Car("나");
+        Cars cars = new Cars(List.of(car1, car2));
+        List<Integer> randomNumbers = List.of(1, 8);
+
+        cars.updateCarsDistance(randomNumbers);
+
+        String actual = car2.toString();
+        String expected = "나 : -";
+
+        assertThat(actual).isEqualTo(expected);
+    }
 
     @Test
     void 단일_우승_자동차_이름_반환() {
@@ -58,6 +74,21 @@ class CarsTest {
 
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> new Cars(cars))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 랜덤숫자_리스트_요소에_null_포함_예외처리() {
+        Car car1 = new Car("가");
+        Car car2 = new Car("나");
+        Cars cars = new Cars(List.of(car1, car2));
+        List<Integer> randomNumbers = new ArrayList<>();
+        randomNumbers.add(null);
+        randomNumbers.add(1);
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> cars.updateCarsDistance(randomNumbers))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
