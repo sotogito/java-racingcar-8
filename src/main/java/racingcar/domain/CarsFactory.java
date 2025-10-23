e2ef84fecd05f7f6c07b3a29504a66d65087705e
sotogito <sotogitoarchive@gmail.com>
2025-10-23 18:12:50 +0900

    feat : CarsFactory 생성

---
+++ src/main/java/racingcar/domain/CarsFactory.java
@@ -0,0 +1,29 @@
+package racingcar.domain;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CarsFactory {
+    private final static String SCAR_NAME_DELIMITER = ",";
+    private final static int CAR_NAME_SPLIT_LIMIT = -1;
+
+    public static Cars create(String inputCarNames) {
+        List<String> carNames = parseCarNames(inputCarNames);
+
+        return new Cars(
+                carNames.stream()
+                        .map(CarsFactory::createCar)
+                        .toList()
+        );
+    }
+
+    private static Car createCar(String carName) {
+        return new Car(carName.trim());
+    }
+
+    private static List<String> parseCarNames(String carNames) {
+        return Arrays.stream(carNames.split(SCAR_NAME_DELIMITER, CAR_NAME_SPLIT_LIMIT))
+                .toList();
+    }
+
+}
